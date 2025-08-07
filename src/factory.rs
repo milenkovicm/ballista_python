b50c0543de8b6387bae1d221a7cb46935bc1564e
Marko Milenković <milenkovicm@users.noreply.github.com>
2025-08-07 11:16:12 +0200

    fix formatting

--- src/factory.rs
+++ src/factory.rs
@@ -73,14 +73,16 @@ def to_miles(km_data):
 
         let result = ctx.sql("select to_miles(a) from t").await?.collect().await?;
 
-        let expected = ["+--------------------+",
+        let expected = [
+            "+--------------------+",
             "| to_miles(t.a)      |",
             "+--------------------+",
             "| 0.62137119         |",
             "| 1.24274238         |",
             "| 1.8641135699999998 |",
             "| 2.48548476         |",
-            "+--------------------+"];
+            "+--------------------+",
+        ];
         assert_batches_eq!(expected, &result);
         Ok(())
     }
